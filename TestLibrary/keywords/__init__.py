c4a96a663424aefe1596f2de2863a02abda41402
jet <251641059@qq.com>
2019-07-15 19:47:37 +0800

    新增关键字

--- TestLibrary/keywords/__init__.py
+++ TestLibrary/keywords/__init__.py
@@ -1,7 +1,8 @@
 from .Persistence import Persistence
 from .Parse import RequestParse
 from .Parse import ExcelParse
+from .Parse import UrlParse
 from .algorithm import crypt
 
-class keywords(Persistence, RequestParse, ExcelParse, crypt):
+class keywords(Persistence, RequestParse, ExcelParse, UrlParse, crypt):
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
