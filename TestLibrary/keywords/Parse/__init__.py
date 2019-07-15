49873366928bf34d15924f6ef0358920907f3479
jet <251641059@qq.com>
2019-07-15 19:47:26 +0800

    新增关键字

--- TestLibrary/keywords/Parse/__init__.py
+++ TestLibrary/keywords/Parse/__init__.py
@@ -1,5 +1,6 @@
 from .RequestParse import RequestParse
 from .ExcelParse import ExcelParse
+from .UrlParse import UrlParse
 
-class Parse(RequestParse,ExcelParse):
+class Parse(RequestParse, ExcelParse, UrlParse):
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
